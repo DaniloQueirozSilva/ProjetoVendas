ffb57ee68b0a61cdcc36685925e6a77f6278ba9c
DaniloQueirozSilva <daniloqueirozsilva05@gmail.com>
2020-02-07 18:17:45 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -15,5 +15,7 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet <Seller> Seller { get; set; }
+        public DbSet <SalesRecord> SalesRecord { get; set; }
     }
 }
