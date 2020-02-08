134c09f64f9d08f57d1b6e068170cf2a2ca560c6
DaniloQueirozSilva <daniloqueirozsilva05@gmail.com>
2020-02-08 13:45:23 -0300

    SellerFormViewModel and Department

--- SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models.ViewModels
 {
-    public class SellerFormViewModel
+    public class SellerFormViewModels
     {
         public Seller Seller { get; set; }
         public ICollection<Department> Departments { get; set; }
